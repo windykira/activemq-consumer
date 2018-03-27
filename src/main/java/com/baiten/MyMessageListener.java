c16282b2b401b4da806eb54b3fbe0e7651654343
马 小磊 <maxl@baiten.cn>
2018-03-27 14:59:49 +0800

    test

--- src/main/java/com/baiten/MyMessageListener.java
+++ src/main/java/com/baiten/MyMessageListener.java
@@ -1,11 +1,9 @@
 package com.baiten;
 
 import com.google.gson.Gson;
+import org.springframework.jms.listener.SessionAwareMessageListener;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.TextMessage;
+import javax.jms.*;
 
 /**
  * Created by windy on 2018/3/26.
@@ -22,8 +20,8 @@ public class MyMessageListener implements MessageListener {
                 String msg = txtMsg.getText();
                 //实际项目中拿到String类型的message(通常是JSON字符串)之后，
                 //会进行反序列化成对象，做进一步的处理
-                System.out.println("receive txt msg===" + msg);
-                int a = 7/0;
+                int i = 7/0;
+                System.out.println("consumer1 receive txt msg===" + msg);
             } catch (JMSException e) {
                 throw new RuntimeException(e);
             }
